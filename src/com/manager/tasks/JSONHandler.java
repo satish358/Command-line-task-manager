d406d000bd54f17a3e1e70bce5e3740d3c8219ee
satish358 <satishmankar745@gmail.com>
2021-05-08 00:08:37 +0530

    File structure (Empty) for project.

---
+++ src/com/manager/tasks/JSONHandler.java
@@ -0,0 +1,4 @@
+package com.manager.tasks;
+
+public class JSONHandler {
+}
---
+++ src/com/manager/tasks/Setup.java
@@ -0,0 +1,10 @@
+package com.manager.tasks;
+
+import java.util.Scanner;
+
+public class Setup {
+    Setup() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("----- \uD83D\uDC90 WELCOME TO TASK MANAGER SETUP \uD83D\uDC90 -----");
+    }
+}
